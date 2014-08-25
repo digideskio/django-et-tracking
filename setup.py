5b54be9b760f23ca7d70a48d22f791f3539f40c5
lexual <lex@jbadigital.com>
2014-08-24 20:33:51 -0700

    conversion support

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_et_tracking',
-    version='0.0.5',
+    version='0.0.6',
     description='Exact Target reporting',
     author='JBA',
     author_email='lex@jbadigital.com',
